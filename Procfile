0b4312b07a3147571cec74e659327f35fe0962f0
Johannes <jkirschnick@gmail.com>
2015-02-03 02:01:52 +0100

    db

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/spring-boot-spinner-0.0.1-SNAPSHOT.jar -Dspring.cloud.appId=talk-spinner
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/spring-boot-spinner-0.0.1-SNAPSHOT.jar -Dspring.cloud.appId=talk-spinner -Dspring.profiles.active=cloud
--- src/main/java/demo/config/CloudConfig.java
+++ src/main/java/demo/config/CloudConfig.java
@@ -1,13 +1,15 @@
 package demo.config;
 
-import org.springframework.cloud.config.java.CloudScan;
+import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Date: 03.02.2015
  *
  */
 @Configuration
-@CloudScan
+@ServiceScan
+@Profile("cloud")
 public class CloudConfig {
 }
