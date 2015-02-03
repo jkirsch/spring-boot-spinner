967a8cec0d5550dc2f37d136abfa10f6fcb6e713
Johannes <jkirschnick@gmail.com>
2015-02-03 14:24:19 +0100

    - service

--- src/main/java/demo/SpringBootSpinnerApplication.java
+++ src/main/java/demo/SpringBootSpinnerApplication.java
@@ -4,9 +4,10 @@ import demo.service.RandomGeneratorService;
 import demo.service.RandomGeneratorViaService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringBootSpinnerApplication {
 
     public static void main(String[] args) {
