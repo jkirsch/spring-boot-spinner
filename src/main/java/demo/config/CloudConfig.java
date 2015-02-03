e03bdd4f14136b40df2099b176316c59fecfd2ac
Johannes <jkirschnick@gmail.com>
2015-02-03 01:20:04 +0100

    db

---
+++ src/main/java/demo/config/CloudConfig.java
@@ -0,0 +1,19 @@
+package demo.config;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Date: 03.02.2015
+ *
+ */
+@Profile("cloud")
+public class CloudConfig  extends AbstractCloudConfig {
+
+    @Bean(destroyMethod = "close")
+    public javax.sql.DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+
+}
