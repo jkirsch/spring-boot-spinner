f5e1320233e24e3bbb6f02f97acfea3044a0a174
Johannes <jkirschnick@gmail.com>
2015-02-03 15:54:44 +0100

    - service

--- src/main/java/demo/config/WebSocketConfig.java
+++ src/main/java/demo/config/WebSocketConfig.java
@@ -1,10 +1,23 @@
 package demo.config;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,6 +32,44 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/spinner").withSockJS();
+    }
+
+    @Bean
+    public Filter encodingFilter() {
+        final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(Charsets.UTF_8.name());
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+    // Another thing that is worth mentioning is that Tomcat only compresses content types of text/html, text/xml and text/plain by default.
+    // Below is an example that supports compression of application/json as well:
+    @Bean
+    public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer servletContainer) {
+                ((TomcatEmbeddedServletContainerFactory) servletContainer).addConnectorCustomizers(
+                        new TomcatConnectorCustomizer() {
+                            @Override
+                            public void customize(Connector connector) {
+                                AbstractHttp11Protocol httpProtocol = (AbstractHttp11Protocol) connector.getProtocolHandler();
+                                httpProtocol.setCompression("on");
+                                httpProtocol.setCompressionMinSize(256);
+                                String mimeTypes = httpProtocol.getCompressableMimeTypes();
+                                String mimeTypesWithJson =
+                                        mimeTypes + "," +
+                                                Joiner.on(",")
+                                                        .join(
+                                                                MediaType.APPLICATION_JSON_VALUE,
+                                                                "application/javascript",
+                                                                "text/css");
+                                httpProtocol.setCompressableMimeTypes(mimeTypesWithJson);
+                            }
+                        }
+                );
+            }
+        };
     }
 
 }
