0c44a27a9dd545aa47d6d6af4e64ffcf00488f13
Johannes <jkirschnick@gmail.com>
2015-02-03 02:06:39 +0100

    db

--- src/main/java/demo/config/CloudConfig.java
+++ src/main/java/demo/config/CloudConfig.java
@@ -1,15 +1,23 @@
 package demo.config;
 
-import org.springframework.cloud.config.java.ServiceScan;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
 
 /**
  * Date: 03.02.2015
  *
  */
 @Configuration
-@ServiceScan
 @Profile("cloud")
-public class CloudConfig {
+public class CloudConfig extends AbstractCloudConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+
 }
