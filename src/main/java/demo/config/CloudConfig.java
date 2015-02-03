35b752f12dd2dfde455293a6daae880a4d7db5bb
Johannes <jkirschnick@gmail.com>
2015-02-03 01:55:43 +0100

    db

---
+++ src/main/java/demo/config/CloudConfig.java
@@ -0,0 +1,13 @@
+package demo.config;
+
+import org.springframework.cloud.config.java.CloudScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Date: 03.02.2015
+ *
+ */
+@Configuration
+@CloudScan
+public class CloudConfig {
+}
